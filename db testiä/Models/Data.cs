b473d58e069b601eeb2dd64a0de962ee03b73c4f
Kasper Suoraniemi <amk1008612@student.hamk.fi>
2025-09-23 14:16:22 +0300

    Nappi AddNew, luoto Data.cs

---
+++ db testiä/Models/Data.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace db_testiä.Models
+{
+    public class Data
+    {
+        [BsonId]  // MongoDB:n ObjectId
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string user_id { get; set; } = string.Empty;
+        public float base_salary { get; set; }
+        public int tax_percentage { get; set; }
+
+        public Bonus[] bonuses;
+        public struct Bonus()
+        {
+          public string category { get; set; }
+          public float amount { get; set; }
+          public bool is_active { get; set; }
+
+        }
+    }
+}
